118b3f3356a10a1ceae8c3164a990742fcee4b9e
Shamima Haque <haqueshamima34@gmail.com>
2024-07-18 00:53:37 +0600

    first commit

---
+++ src/components/includes/MyFooter.vue
@@ -0,0 +1,13 @@
+<template>
+  
+    <router-view/>
+  </template>
+  
+  <script>
+  export default{
+    name: 'MyFooter'
+   }
+  </script>
+  <style scoped>
+  </style>
+  
---
+++ src/components/includes/MyNavbar.vue
@@ -0,0 +1,13 @@
+<template>
+  
+    <router-view/>
+  </template>
+  
+  <script>
+  export default{
+    name: 'MyNavbar'
+   }
+  </script>
+  <style scoped>
+  </style>
+  
