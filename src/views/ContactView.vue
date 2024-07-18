e9b9bbaf39c6c1f0da700b449024efc51be99c29
Shamima Haque <haqueshamima34@gmail.com>
2024-07-18 14:53:47 +0600

    mastering done

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,16 +1,19 @@
 <template>
     <MyPageTitle/>
     <MyContact/>
+    <MyMap/>
   
     </template>
     
     <script>
     import MyPageTitle from "@/components/contact/MyPageTitle";
     import MyContact from "@/components/contact/MyContact";
+    import MyMap from "@/components/contact/MyMap";
+    
     
     export default{
         name: 'ContactView',
-        components:{MyPageTitle,MyContact}
+        components:{MyPageTitle,MyContact,MyMap}
     }
     </script>
     
